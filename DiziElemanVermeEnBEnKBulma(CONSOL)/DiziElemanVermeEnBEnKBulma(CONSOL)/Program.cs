23923375c18747a0127191408b9002d8cbcdef20
Mustafa Yıldırım <39571054+rootercs@users.noreply.github.com>
2018-10-03 00:48:44 +0300

    Project Added

---
+++ DiziElemanVermeEnBEnKBulma(CONSOL)/DiziElemanVermeEnBEnKBulma(CONSOL)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiziElemanVermeEnBEnKBulma_CONSOL_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            int max, min;
+            char c;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Dizinin Uzunluğunu Giriniz");
+                int length = Convert.ToInt32(Console.ReadLine());
+                numbers = new int[length];
+
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine("A[{0}]=", i);
+                    numbers[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                max = numbers[0]; min = numbers[0];
+                foreach (int item in numbers)
+                {
+                    if (max < item) max = item;
+                    if (min > item) min = item;
+                }
+
+                Console.WriteLine("dizinin en büyük degeri {0} ' dir.", max);
+                Console.WriteLine("dizinin en kücük degeri {0} ' dir.", min);
+
+                Console.WriteLine("devam etmek istermisiniz e/h");
+                c = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (c=='h')
+                {
+                    Environment.Exit(1);
+                }
+            } while (c=='e');       
+
+
+        }
+    }
+}
---
+++ DiziElemanVermeEnBEnKBulma(CONSOL)/DiziElemanVermeEnBEnKBulma(CONSOL)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("DiziElemanVermeEnBEnKBulma(CONSOL)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiziElemanVermeEnBEnKBulma(CONSOL)")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("e2e671cb-b5a7-4948-8e78-0ad011b6bf42")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
